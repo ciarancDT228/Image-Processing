2f0e3f20e9fe5ea3f72bb047dc2169a867c5ec42
Ciaran Carroll <D15124233@mydit.ie>
2020-09-28 22:52:52 +0100

    first commit

---
+++ Task1 Access Pixels/task1.py
@@ -0,0 +1,7 @@
+%matplotlib inline
+
+import cv2 # The OpenCV library; install using `pip install opencv-contrib-python`
+import numpy as np # Helpful when working with arrays; install using `pip install numpy`
+from matplotlib import pyplot as plt # Good for graphing; install using `pip install matplotlib`
+from matplotlib import image as image
+import easygui # An easy-to-use file-picker; pip install easygui
